075b92e5e8c4ff93b12fe2514d77b15dc16c7b3d
João Pedro -JFI <jfi@certi.org.br>
2022-11-27 08:38:54 -0400

    correction in types

--- adonis-typings/kafka.ts
+++ adonis-typings/kafka.ts
@@ -1,23 +1,26 @@
-export interface KafkaConfig {
-  enabled: string
-  clientId: string
-  groupId: string
-  url: string
-  port: number
-  urls?: string
-  fromBeginning: boolean
-  autoCommit: boolean
-  connectionTimeout?: number
-  requestTimeout?: number
-  partitionsConcurrently?: number
-  logLevel: any
-}
+declare module '@ioc:Message/Kafka' {
+  export interface KafkaConfig {
+    enabled: string
+    clientId: string
+    groupId: string
+    url: string
+    port: number
+    urls?: string
+    fromBeginning: boolean
+    autoCommit: boolean
+    connectionTimeout?: number
+    requestTimeout?: number
+    partitionsConcurrently?: number
+    logLevel: any
+  }
 
-export interface KafkaContract {
-  start?: (...args: any[]) => void
-  on?: (...args: any[]) => void
-  send?: (topic: string, data: object) => void
-  disconnect?: () => void
-}
+  export interface KafkaContract {
+    start?: (...args: any[]) => void
+    on?: (...args: any[]) => void
+    send?: (topic: string, data: object) => void
+    disconnect?: () => void
+  }
 
-export * from 'kafkajs'
+  export * from 'kafkajs'
+
+}
