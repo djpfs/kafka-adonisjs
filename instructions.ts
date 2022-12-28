fec99045c6e05ce0eda177636e14906faf6330ad
João Pedro -JFI <jfi@certi.org.br>
2022-12-28 10:25:12 -0400

    instructions

--- instructions.ts
+++ instructions.ts
@@ -6,38 +6,59 @@ function getStub(...relativePaths: string[]) {
   return join(__dirname, 'templates', ...relativePaths)
 }
 
-export default async function instructions(
-  projectRoot: string,
-  app: ApplicationContract,
-  sink: typeof sinkStatic
-) {
-  const configPath = app.configPath('kafka.ts')
+function makeConfig(projectRoot: string, app: ApplicationContract, sink: typeof sinkStatic) {
+  const configDirectory = app.directoriesMap.get('config') || 'config'
+  const configPath = join(configDirectory, 'kafka.ts')
   const kafkaConfig = new sink.files.MustacheFile(projectRoot, configPath, getStub('config.txt'))
 
-  kafkaConfig.overwrite = true
-
+  if (kafkaConfig.exists()) {
+    sink.logger.action('skip').succeeded(configPath)
+    return
+  }
   kafkaConfig.commit()
+  sink.logger.action('create').succeeded(configPath)
 
-  const configDir = app.directoriesMap.get('config') || 'config'
-  sink.logger.action('create').succeeded(`${configDir}/kafka.ts`)
+}
 
+function makeContract(projectRoot: string, app: ApplicationContract, sink: typeof sinkStatic) {
   const contractsPath = app.makePath('contracts/kafka.ts')
   const kafkaContract = new sink.files.MustacheFile(
     projectRoot,
     contractsPath,
     getStub('contract.txt')
   )
-  kafkaContract.overwrite = true
 
+  if (kafkaContract.exists()) {
+    sink.logger.action('skip').succeeded('contracts/kafka.ts')
+    return
+  }
   kafkaContract.commit()
-
   sink.logger.action('create').succeeded('contracts/kafka.ts')
+}
 
+function makeStart(projectRoot: string, app: ApplicationContract, sink: typeof sinkStatic) {
   const startPath = app.makePath('start/kafka.ts')
   const kafkaStart = new sink.files.MustacheFile(projectRoot, startPath, getStub('start.txt'))
-  kafkaStart.overwrite = true
 
+  if (kafkaStart.exists()) {
+    sink.logger.action('skip').succeeded('start/kafka.ts')
+    return
+  }
   kafkaStart.commit()
-
   sink.logger.action('create').succeeded('start/kafka.ts')
+}
+
+export default async function instructions(
+  projectRoot: string,
+  app: ApplicationContract,
+  sink: typeof sinkStatic
+) {
+  sink.getPrompt().ask('Do you want install kafka nodejs client?', {
+    validate(_) {
+      makeConfig(projectRoot, app, sink)
+      makeContract(projectRoot, app, sink)
+      makeStart(projectRoot, app, sink)
+      return true
+    },
+  });
 }
